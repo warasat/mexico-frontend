850b09171f016ce4aacb5a45782b1a992c4471db
hafiztalharazafarooqi <hafiztalharazafarooqi@gmail.com>
2025-09-05 01:14:30 +0500

    change

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react/template/', // 👈 required for sub-path deployment
+  base: "/react/template/", // for subpath deployment
   resolve: {
     alias: {
-      '@': '/src',
-      moment: 'moment/moment.js'
+      "@": "/src",
+      moment: "moment/moment.js",
     },
   },
-})
+});
