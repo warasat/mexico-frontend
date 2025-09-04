9d0e82a0cd3a26dafe34720a80d596cd78726948
hafiztalharazafarooqi <hafiztalharazafarooqi@gmail.com>
2025-09-05 00:37:50 +0500

    fix changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/react/template/', // 👈 This ensures correct asset loading path
+   base: '/', // 👈 This ensures correct asset loading path
    resolve: {
     alias: {
       moment: 'moment/moment.js'
